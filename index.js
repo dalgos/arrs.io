2c4fba360d445bfa69e774d32319ae7b69198d8b
Kwangosoo Shin <dalgos@dalgos.com>
2015-07-06 02:55:01 +0900

    index.html

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
-/* @flow */
-// var request = require('request');
-// var cheerio = require('cheerio');
-//
-// var opt = {
-//   urls : {
-//     'news' : 'http://clien.net/cs2/bbs/board.php?bo_table=news'
-//   }
-// };
-//
-// request(opt.urls.news, function(err, res, html){
-//   if(!err && res.statusCode == 200){
-//     var _ = cheerio.load(html);
-//     _('td.post_subject').each(function(idx, el){
-//       var a = _(this);
-//       console.log(a.text());
-//     });
-//   }
-// });
-var express = require('express');
-var app = express();
+var http = require('http'),
+    fs = require('fs');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
 
-var server = app.listen(3000, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    http.createServer(function(request, response) {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }).listen(80);
 });
